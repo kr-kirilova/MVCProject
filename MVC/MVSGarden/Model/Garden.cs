8935feff94297c0094f0ff3533f514143c8c952a
kr-kirilova <k.kirilova2019@pmg-pernik.com>
2023-02-13 11:06:38 +0200

    add model

---
+++ MVC/MVSGarden/Model/Garden.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVSGarden.Model
+{
+   public  class Garden
+    {
+        public decimal PriceVegitables { get; set; }
+        public decimal PriceFruits { get; set; }
+        public int KgVegitable { get; set; }
+        public int KgFruits { get; set; }
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.KgFruits * this.PriceFruits + this.KgVegitable * this.PriceVegitables);
+        }
+    }
+}
