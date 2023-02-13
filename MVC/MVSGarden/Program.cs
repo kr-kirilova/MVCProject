af87cfd6c0a59f858cbd70bae8d0c137cab42992
kr-kirilova <k.kirilova2019@pmg-pernik.com>
2023-02-13 10:20:52 +0200

    upload project

---
+++ MVC/MVSGarden/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVSGarden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
